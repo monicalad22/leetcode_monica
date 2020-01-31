3b074ed00ca76ece8419b721ef35a71037944b9e
monicalad22 <59317669+monicalad22@users.noreply.github.com>
2020-01-30 22:03:12 -0600

    Update 20.ValidParentheses.py
    spaces reduced

--- 20.ValidParentheses.py
+++ 20.ValidParentheses.py
@@ -2,7 +2,7 @@ class Solution():
     def isValid(self, s):
         # Until the while condition is true.
         while '()' in s or '[]' in s or '{}' in s:
-            s = s.replace('()', '').replace('{}', '').replace('[]', '')
+            s = s.replace('()','').replace('{}','').replace('[]','')
         return s == ''
 
 a = Solution()
